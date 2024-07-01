a810afea9815ffe2a45736b18c6babd4e2fb5a90
Meenatchi Sundaram Muthu Selva Annamalai <meenatchi.annamalai.22@ucl.ac.uk>
2024-07-01 05:51:41 +0000

    Confirmed dependencies fixed

--- libs/install.sh
+++ libs/install.sh
@@ -9,4 +9,3 @@ pip install -e libs/opendp/
 pip install git+https://github.com/ryan112358/private-pgm.git
 pip install -e libs/optimized_qbs/
 pip install opacus==1.4.0
-pip install scikit-learn==1.0.2
